3b0b53491acb45a34df7990c48befa5de778b2e7
abanerjee-06 <aritra.b2000@outlook.com>
2021-03-27 16:37:23 +0300

    Adding colorize.py

---
+++ colorize.py
@@ -0,0 +1,52 @@
+from skimage import io, color
+from src import colnet
+from src import dataset
+from src import utils
+
+import os
+import argparse
+import torch
+import torchvision.transforms
+import numpy as np
+
+def colorize(path,model):
+    checkpt = torch.load(model,map_location=torch.device("cpu"))
+    net_divisor = checkpt['net_divisor']
+    class = checkpt['class']
+    n_class = len(class)
+
+    net = colnet.ColNet(num_class=n_class,net_divisor = net_divisor)
+    net.load_state_dict(checkpt['model_state_dict'])
+
+    transforms = torchvision.transforms.Compose(
+        [dataset.HandleGrayscale(),
+        dataset.RandomCrop(224),
+        dataset.Rgb2LabNorm(), 
+        dataset.ToTensor(), 
+        dataset.SplitLab()])
+    
+    img = io.imread(path)
+
+    L,ab = transforms(img)
+    L_tensor = torch.from_numpy(np.expand_dims(L,axis=0))
+
+    softmax = torch.nn.Softmax(dim=1)
+    net.eval()
+    with torch.no_grad():
+        ab_out,pred = net(L_tensor)
+        img_color = utils.net2RGB(L,ab_out[0])
+        io.imsave("colorized-"+os.path.basename(path))
+
+        probs = softmax(pred)[0].numpy()
+        probs_class = sorted(zip(probs,class),key=lamda x:x[0],reverse=True)
+
+        print('Predicted labels: \n')
+        for p,c in probs_class[:10]:
+            print("{:>7.2f}% \t{}".format(p*100.0, c))
+        
+parser = argparse.ArgumentParser(description="A script to colorize a photo")
+parser.add_argument('image', help="Path to the image. RGB one will be converted to grayscale")
+parser.add_argument('model', help="Path a *.pt model")
+args = parser.parse_args()
+print("[Warrning] Only 224x224 images are supported. Otherwise an image will be randomly cropped")
+colorize(args.image, args.model)    
