c2152cd8b7f9e95800cd62d870bc2d2efcbee855
Jayati <jayatinarang@gmail.com>
2021-04-22 21:47:01 +0530

    updated train class

--- src/trainer.py
+++ src/trainer.py
@@ -1,17 +1,12 @@
-import os
-import shutil
-import time
-
-import torch
+import os, shutil, time, torch
 import torch.nn as nn
 import torch.optim as optim
-
-from torch.utils.data import DataLoader
 from skimage import io
-
+from torch.utils.data import DataLoader
+from .utils import net2RGB
 from .colnet import ColNet
 from .dataset import ImagesDateset
-from .utils import net2RGB
+
 
 class Train:
     def __init__(self,batch_size,epochs,img_dir_train, img_dir_val,img_dir_test,start_epoch=0,learning_rate=0.0001,
@@ -68,9 +63,7 @@ class Train:
         self.models_dir = models_dir
     
     def loss(self,class_target,class_out,col_target,col_out):
-        loss_class = self.ce(class_out,class_target)
-        loss_col = self.mse(col_target,col_out)
-        return loss_col + loss_class/300
+        return self.mse(col_target,col_out) + self.ce(class_out,class_target)/300
     
     def train(self,epoch):
         epoch_loss = 0
@@ -78,18 +71,17 @@ class Train:
 
         for idx in range(len(self.trainloader)):
             L, ab, labels = self.trainloader[idx]
-            L, ab, labels = L.to(self.device), ab.to(self.device), labels.to(self.device)
+            L = L.to(self.device)
+            ab = ab.to(self.device)
+            labels = labels.to(self.device)
             self.optimizer.zero_grad()
             ab_out, labels_out = self.net(L)
 
             loss = self.loss(ab, ab_out, labels, labels_out)
             loss.backward()
             self.optimizer.step()
-
             epoch_loss += loss.item()
-
-            print('[Epoch {:>2} / {} | Batch: {:>2} / {}] loss: {:>10.3f}'.format(epoch+1, self.EPOCHS, idx + 1, len(self.trainloader), loss.item()))
-        
+            print('[Epoch ' + str(epoch+1) + '/' + str(self.EPOCHS) + ' | Batch: ' + str(idx +1) + '/' + str(len(self.trainloader)) + ']' + ' loss: ' + str(loss.item()))
         epoch_loss /= len(self.trainloader)
         self.loss_history['train'].append(epoch_loss)
 
@@ -101,15 +93,14 @@ class Train:
         with torch.no_grad():
             for idx in range(len(self.devloader)):
                 L_dev, ab_dev, labels_dev = self.devloader[idx]
-                L_dev, ab_dev, labels_dev = L_dev.to(self.device), ab_dev.to(self.device), labels_dev.to(self.device)
-                
+                L_dev = L_dev.to(self.device)
+                ab_dev = ab_dev.to(self.device)
+                labels_dev = labels_dev.to(self.device)
                 ab_dev_out,labels_dev_out = self.net(L_dev)
 
                 dev_batch_loss = self.loss(ab_dev,ab_dev_out,labels_dev,labels_dev_out)
                 dev_loss += dev_batch_loss.item()
-
-                print("[Validation] [Batch {:>2} / {}] dev loss: {:>10.3f}".format(idx+1, len(self.devloader), dev_batch_loss))
-            
+                print('[Validation] [Batch ' + str(idx +1) + '/' + str(len(self.devloader)) + ']' + ' dev loss: ' + str(dev_batch_loss))        
             dev_loss /= len(self.devloader)
             self.loss_history['val'].append(dev_loss)
             print("Dev loss ",dev_loss)
@@ -133,11 +124,10 @@ class Train:
             'losses': self.loss_history,'classes': self.classes}, full_path)
 
         self.current_model_name = full_path
-        print('\nsaved model to {}\n'.format(full_path))
+        print('\nSaved the model to', full_path)
 
-        current_val_loss = self.loss_history['val'][-1]
-        if (current_val_loss < self.best_val_loss):
-            self.best_val_loss = current_val_loss
+        if (self.loss_history['val'][-1] < self.best_val_loss):
+            self.best_val_loss = self.loss_history['val'][-1]
             shutil.copy(full_path, self.best_model_dir)
             print("Saved the best model on epoch: ",epoch+1,"\n")
 
@@ -145,10 +135,11 @@ class Train:
         print("Resuming training of: " + model_checkpoint)
         checkpoint = torch.load(model_checkpoint, map_location=self.device)
         self.net.load_state_dict(checkpoint['model_state_dict'])
-        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         self.loss_history = checkpoint['losses']
-        self.start_epoch = checkpoint['epoch'] + 1 
         self.current_model_name = model_checkpoint
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        self.start_epoch = checkpoint['epoch'] + 1 
+        
 
     def run(self):
         for epoch in range(self.start_epoch, self.EPOCHS):
