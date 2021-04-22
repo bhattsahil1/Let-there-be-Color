2c4bdedc097605aa6bf3372aba71f1674c79415a
SAHIL BHATT <42675395+bhattsahil1@users.noreply.github.com>
2021-04-22 18:11:48 +0530

    modified trainer code; new loss functions need to be added

--- src/trainer.py
+++ src/trainer.py
@@ -14,7 +14,7 @@ from .dataset import ImagesDateset
 from .utils import net2RGB
 
 class Train:
-    def __init__(self,batch_size,epochs,img_dir_train, img_dir_val,img_dir_test,start_epoch=0,net_divisor=1,learning_rate=0.0001,
+    def __init__(self,batch_size,epochs,img_dir_train, img_dir_val,img_dir_test,start_epoch=0,learning_rate=0.0001,
         model_checkpoint=None,models_dir='./model/',img_out_dir='./out',num_workers=4):
 
         if os.path.exists(models_dir) == False:
@@ -30,8 +30,7 @@ class Train:
         
         self.classes = self.trainloader.dataset.classes
         self.num_classes = len(self.classes)
-        self.BATCH_SIZE = BATCH_SIZE
-        self.net_divisor = net_divisor
+        self.BATCH_SIZE = batch_size
         self.EPOCHS = epochs
         self.start_epoch = start_epoch
 
@@ -40,11 +39,11 @@ class Train:
         self.devloader = DataLoader(self.devset, batch_size=self.BATCH_SIZE,
                                     shuffle=False, num_workers=num_workers)
 
-        self.img_dir_train = self.img_dir_train
+        self.img_dir_train = img_dir_train
         self.trainset = ImagesDateset(self.img_dir_train)
         self.trainloader = DataLoader(self.trainset, batch_size=self.BATCH_SIZE, 
                                       shuffle=True, num_workers=num_workers)
-        self.img_dir_test = self.img_dir_test
+        self.img_dir_test = img_dir_test
         self.testset = ImagesDateset(self.img_dir_test, testing=True)
         self.testloader = DataLoader(self.testset, batch_size=self.BATCH_SIZE,
                                      shuffle=False, num_workers=num_workers)
@@ -54,11 +53,17 @@ class Train:
         self.ce = nn.CrossEntropyLoss()
         self.optimizer = optim.Adam(self.net.parameters(), lr=learning_rate)
         
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() 
-                                   else "cpu")        
-        self.net = ColNet(net_divisor=net_divisor, num_classes=self.num_classes)
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")        
+        self.net = ColNet(num_classes=self.num_classes)
         self.net.to(self.device)
         print("Using {}\n".format(self.device))
+        if model_checkpoint:
+            self.load_checkpoint(model_checkpoint)
+        
+        self.current_model_name = model_checkpoint
+        self.best_val_loss = float("inf")
+        self.best_model_dir = os.path.join(self.models_dir, 'colnet-the-best.pt')
+        
         self.img_out_dir = img_out_dir
         self.models_dir = models_dir
     
@@ -72,12 +77,12 @@ class Train:
         self.net.train()
 
         for idx in range(len(self.trainloader)):
-            L,ab,label = self.trainloader[idx]
+            L, ab, labels = self.trainloader[idx]
             L, ab, labels = L.to(self.device), ab.to(self.device), labels.to(self.device)
             self.optimizer.zero_grad()
-            ab_out,labels_out = self.net(L)
+            ab_out, labels_out = self.net(L)
 
-            loss = self.loss(ab,ab_out,labels,labels_out)
+            loss = self.loss(ab, ab_out, labels, labels_out)
             loss.backward()
             self.optimizer.step()
 
@@ -95,8 +100,8 @@ class Train:
         self.net.eval()
         with torch.no_grad():
             for idx in range(len(self.devloader)):
-                L_dev,ab_dev,labels_dev = self.devloader[idx]
-                L_dev,ab_dev,labels_dev = L_dev.to(self.device),ab_dev.to(self.device),labels_dev.to(self.device)
+                L_dev, ab_dev, labels_dev = self.devloader[idx]
+                L_dev, ab_dev, labels_dev = L_dev.to(self.device), ab_dev.to(self.device), labels_dev.to(self.device)
                 
                 ab_dev_out,labels_dev_out = self.net(L_dev)
 
@@ -110,41 +115,22 @@ class Train:
             print("Dev loss ",dev_loss)
     
     def test(self,model_dir):
-        if (model_dir is None):
+        if model_dir is None:
             model_dir = self.current_model_name
+
             if os.path.isfile(self.best_model_dir):
                 model_dir = self.best_model_dir
         
         self.load_checkpoint(model_dir)
         self.net.to(self.device)
         self.net.eval()
-        
-        with torch.no_grad():
-            for idx in range(len(self.testloader)):
-                L,_,labels = self.testloader[idx]
-                L = L.to(self.device)
-                ab_out,_ = self.net(L)
-
-                L = L.to(torch.device("cpu"))
-                ab_out = ab_out.to(torch.device("cpu"))
-
-                for i in range(L.shape[0]):
-                    img = net2RGB(L[i],ab_out[i])
-                    io.imsave(os.path.join(self.img_out_dir,labels[i]),img)
-
-        print("Saved all photos to "+self.img_out_dir)
 
     def save_checkpoint(self,epoch):
         full_path = os.path.join(self.models_dir,"colnet{}-{}.pt".format(time.strftime("%y%m%d-%H-%M-%S"), epoch))
 
         torch.save({
-            'epoch':epoch,
-            'model_state_dict': self.net.state_dict(),
-            'optimizer_state_dict': self.optimizer_state_dict(),
-            'losses': self.loss_history,
-            'net_divisor': self.net_divisor,
-            'classes': self.classes
-        },full_path)
+            'epoch': epoch,'model_state_dict': self.net.state_dict(),'optimizer_state_dict': self.optimizer.state_dict(),
+            'losses': self.loss_history,'classes': self.classes}, full_path)
 
         self.current_model_name = full_path
         print('\nsaved model to {}\n'.format(full_path))
@@ -156,45 +142,23 @@ class Train:
             print("Saved the best model on epoch: ",epoch+1,"\n")
 
     def load_checkpoint(self,model_checkpoint):
-        print('Resume training of: '+model_checkpoint)
-        checkpoint = torch.load(model_checkpoint, map_location=torch.device("cpu"))
+        print("Resuming training of: " + model_checkpoint)
+        checkpoint = torch.load(model_checkpoint, map_location=self.device)
         self.net.load_state_dict(checkpoint['model_state_dict'])
         self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         self.loss_history = checkpoint['losses']
         self.start_epoch = checkpoint['epoch'] + 1 
-        self.net_divisor = checkpoint['net_divisor'] 
         self.current_model_name = model_checkpoint
 
     def run(self):
-        for epoch in range(self.start_epoch,self.EPOCHS):
-            print("{2}\nEpoch {0} / {1}\n{2}"
-                  .format(epoch + 1, self.EPOCHS, '-'*47))
+        for epoch in range(self.start_epoch, self.EPOCHS):
+            print("{2}\nEpoch {0} / {1}\n{2}".format(epoch + 1, self.EPOCHS, '-'*47))
             self.train(epoch)
             self.validate(epoch)
             self.save_checkpoint(epoch)
         print('\nFinished Training.\n')
-    
-    def info(self):
-        print("{0} Training environment info {0}\n".format("-"*13))
 
-        print("Training starts from epoch: {}".format(self.start_epoch))
-        print("Total number of epochs:     {}".format(self.EPOCHS))
-        print("ColNet parameters are devided by: {}".format(self.net_divisor))
-        print("Batch size:  {}".format(self.BATCH_SIZE))
-        print("Used devide: {}".format(self.device))
-        print("Number of classes: {}".format(self.num_classes))
-        print()
-
-        if self.current_model_name:
-            print("Current model name:      " + self.current_model_name)
-
-        print("Training data directory: " + self.img_dir_train)
-        print("Validate data directory: " + self.img_dir_val)
-        print("Testing data directory:  " + self.img_dir_test)
-        print("Models are saved to:     " + self.models_dir)
-        print("Colorized images are saved to: " + self.img_out_dir)
-        print("-" * 53 + "\n")
 
 if __name__ == "__main__":
-    print("Hello, have a great day!")
+    print("Training model")
             
